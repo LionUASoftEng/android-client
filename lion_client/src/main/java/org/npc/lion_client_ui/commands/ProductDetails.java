d9c9a11f7754e8af7eb739d3c37f2e4f01895477
bpalexa <bpalexa@email.uark.edu>
2015-11-17 19:06:15 -0600

    Product details buttons, new textviews for price/quantity

--- lion_client/src/main/java/org/npc/lion_client_ui/commands/ProductDetails.java
+++ lion_client/src/main/java/org/npc/lion_client_ui/commands/ProductDetails.java
@@ -1,18 +1,18 @@
 package org.npc.lion_client_ui.commands;
 
+import android.os.AsyncTask;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
+import org.npc.lion_client_ui.api.enums.ProductApiRequestStatus;
+import org.npc.lion_client_ui.api.models.Product;
+import org.npc.lion_client_ui.api.services.ProductService;
 import org.npc.lion_client_ui.R;
 
 public class ProductDetails extends AppCompatActivity {
 
-    public String lookup_code = null;
     public String description = "Product not found!";
-    public String price = "$0.00";
-    public String quantity = "0 units";
     TextView lookup_code_textview;
     TextView description_content;
     TextView price_textview;
@@ -23,21 +23,46 @@ public class ProductDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
-        //TODO Get product info from server and put it into strings
         Intent intent = getIntent();
         lookup_code = intent.getStringExtra(
                 this.getResources().getString(R.string.lookup_code_extras_key)
         );
 
         lookup_code_textview = (TextView) findViewById(R.id.lookup_code_textView);
-        lookup_code_textview.setText(R.string.lookup_code);
         description_content = (TextView) findViewById(R.id.description_content_textview);
-        description_content.setText( description );
         price_textview = (TextView) findViewById(R.id.price_textview);
-        price_textview.setText(R.string.price);
         quantity_textview = (TextView) findViewById(R.id.quantity_textview);
-        quantity_textview.setText(R.string.quantity);
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //*********************************
+        //commented out temporarily because without a valid UUID the app will crash here
+        //
+        //(new RetrieveProductTask()).execute(this.lookup_code);
+    }
+
+    private String lookup_code;
+    private class RetrieveProductTask extends AsyncTask<String, Void, Product>{
+        protected Product doInBackground(String... lookupCodes){
+            return (new ProductService()).getProductbyLookupCode(lookupCodes[0]);
+        }
+
+        protected void onPostExecute(Product result) {
+            if (result.getApiRequestStatus() == ProductApiRequestStatus.OK) {
+                getLookupCodeTextView().setText(result.getLookupCode());
+                getDescriptionTextView().setText(result.getDescription());
+                String dPrice = Double.toString(result.getPrice());
+                getPriceTextView().setText(dPrice);
+                String iQuantity = Integer.toString(result.getQuantity());
+                getQuantityTextView().setText(iQuantity);
+            } else {
+                getLookupCodeTextView().setText(result.getApiRequestStatus().name());
+            }
+        }
     }
 
     private TextView getLookupCodeTextView() {
